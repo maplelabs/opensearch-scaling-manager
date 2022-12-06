741272e31dd3c059e305b88cd323d93af46c80ae
ubuntu <meghanar1998@gmail.com>
2022-12-06 05:16:58 +0000

    Revert "Merge branch 'release_v0.1' into provisioner"
    This reverts commit a18c767d4cb6750ad994a0fa7975832b6d715e3d, reversing
    changes made to 41ee35e83fb5668e891509cded5a4ff6064d1926.

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"scaling_manager/task"
 	"strings"
 	"time"
-
-	"github.com/fsnotify/fsnotify"
 )
 
 var State = new(provision.State)
@@ -18,22 +16,14 @@ func main() {
 	// A periodic check if there is a change in master node to pick up incomplete provisioning
 	go periodicProvisionCheck()
 	// The polling interval is set to 5 minutes and can be configured.
-	go fileWatch()
-	ticker := time.Tick(300 * time.Second)
+	ticker := time.Tick(5 * time.Second)
 	for range ticker {
 		// This function is responsible for fetching the metrics and pushing it to the index.
 		// In starting we will call simulator to provide this details with current timestamp.
 		// fetch.FetchMetrics()
 		// This function will be responsible for parsing the config file and fill in task_details struct.
 		var task = new(task.TaskDetails)
-		configStruct, err := config.GetConfig("config.yaml")
-		if err != nil {
-			fmt.Println("The recommendation can not be made as there is an error in the validation of config file.")
-			fmt.Println(err)
-			continue
-		} else {
-			fmt.Println(configStruct)
-		}
+		configStruct := config.GetConfig("config.yaml")
 		task.Tasks = configStruct.TaskDetails
 		// This function is responsible for evaluating the task and recommend.
 		recommendation_list := task.EvaluateTask()
@@ -47,7 +37,7 @@ func main() {
 // Output:
 
 func periodicProvisionCheck() {
-	tick := time.Tick(30 * time.Second)
+	tick := time.Tick(5 * time.Second)
 	previous_master := cluster.GetCurrentMasterIp()
 	for range tick {
 		current_state := State.GetCurrentState()
@@ -87,35 +77,3 @@ func periodicProvisionCheck() {
 	}
 }
 
-func fileWatch() {
-	//Adding file watcher to detect the change in configuration
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		fmt.Println("ERROR", err)
-	}
-	defer watcher.Close()
-	done := make(chan bool)
-
-	//A go routine that keeps checking for change in configuration
-	go func() {
-		for {
-			select {
-			// watch for events
-			case event := <-watcher.Events:
-				//If there is change in config then clear recommendation queue
-				//clearRecommendationQueue()
-				fmt.Printf("EVENT! %#v\n", event)
-				fmt.Println("The recommendation queue will be cleared.")
-			case err := <-watcher.Errors:
-				fmt.Println("ERROR in file watcher", err)
-			}
-		}
-	}()
-
-	// Adding fsnotify watcher to keep track of the changes in config file
-	if err := watcher.Add("config.yaml"); err != nil {
-		fmt.Println("ERROR", err)
-	}
-
-	<-done
-}
