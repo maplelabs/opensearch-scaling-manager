caa2ab9e22a5dde029ab63a40615e98dd709d561
Manoj Kumar Chandru <Manojkumar.Chandru@maplelabs.com>
2022-12-16 18:50:49 +0530

    Added test cases for testing parse_config

--- simulator/src/constants.py
+++ simulator/src/constants.py
@@ -1,35 +1,33 @@
-
 # file paths
-SCHEMA_FILE_NAME = 'schema.py'
-CONFIG_FILE_PATH = 'config.yaml'
-PROVISION_LOCK_FILE_NAME = 'provision.lock'
-SIMULATION_GRAPH_FILE_NAME = 'simulation_diagram.png'
+SCHEMA_FILE_NAME = "schema.py"
+CONFIG_FILE_PATH = "config.yaml"
+PROVISION_LOCK_FILE_NAME = "provision.lock"
+SIMULATION_GRAPH_FILE_NAME = "simulation_diagram.png"
 
 # config keys
 DATA_INGESTION = "data_ingestion"
-DATA_INGESTION_STATES = 'states'
+DATA_INGESTION_STATES = "states"
 SEARCHES = "searches"
 DATA_GENERATION_INTERVAL_MINUTES = "data_generation_interval_minutes"
-DATA_INGESTION_RANDOMNESS_PERCENTAGE = 'randomness_percentage'
+DATA_INGESTION_RANDOMNESS_PERCENTAGE = "randomness_percentage"
 # Todo - Shrinidhi/Manoj add remaining keys from config yaml
 
-CLUSTER_STATE_GREEN = 'green'
-CLUSTER_STATE_YELLOW = 'yellow'
-CLUSTER_STATE_RED = 'red'
+CLUSTER_STATE_GREEN = "green"
+CLUSTER_STATE_YELLOW = "yellow"
+CLUSTER_STATE_RED = "red"
 
 CLUSTER_STATES = [CLUSTER_STATE_GREEN, CLUSTER_STATE_YELLOW, CLUSTER_STATE_RED]
 
 HIGH_INGESTION_RATE_GB_PER_HOUR = 60
 
 # mapping inputs for API endpoints
-CLUSTER_STATE = 'status'
-CPU_USAGE_PERCENT = 'cpu_usage_percent'
-MEMORY_USAGE_PERCENT = 'memory_usage_percent'
+CLUSTER_STATE = "status"
+CPU_USAGE_PERCENT = "cpu_usage_percent"
+MEMORY_USAGE_PERCENT = "memory_usage_percent"
 STAT_REQUEST = {
-    'cpu': CPU_USAGE_PERCENT,
-    'mem': MEMORY_USAGE_PERCENT,
-    'status': CLUSTER_STATE
-
+    "cpu": CPU_USAGE_PERCENT,
+    "mem": MEMORY_USAGE_PERCENT,
+    "status": CLUSTER_STATE,
 }  # Todo : Shrinidhi/Manoj Add remaining stats
 
 APP_PORT = 5000
--- simulator/tests/test_config_parser.py
+++ simulator/tests/test_config_parser.py
@@ -1,69 +1,106 @@
 import sys
-import yaml
+import os
 from pathlib import Path
+from unittest import expectedFailure
+import yaml
+import pytest
 
-current_file: Path = Path(__file__).parent.parent.resolve()
-path = str(current_file) + "\src"
-print(path)
+
+current_file = Path(__file__).parent.parent.resolve()
+test_path = str(os.path.join(str(current_file), "tests", "config_test"))
+path = os.path.join(str(current_file), "src")
 sys.path.insert(0, path)
 
-from config_parser import validate_config
+from config_parser import validate_config, parse_config, Config
+from errors import ValidationError
+from constants import CONFIG_FILE_PATH
+import constants as const
 
 
 def test_validate_config():
-    is_valid, errors = validate_config(
-        yaml.safe_load(open("./config_test/config_1_P.yaml", "r"))
-    )
-    assert is_valid == True
-    assert errors == {}
+    with open(os.path.join(test_path, "config_1_P.yaml"), "r") as file:
+        is_valid, errors = validate_config(yaml.safe_load(file))
+        assert is_valid == True
+        assert errors == {}
 
 
 def test_validate_config_without_searches():
-    is_valid, errors = validate_config(
-        yaml.safe_load(open("./config_test/config_2_P.yaml", "r"))
-    )
-    assert is_valid == True
-    assert errors == {}
+    with open(os.path.join(test_path, "config_2_P.yaml"), "r") as file:
+        is_valid, errors = validate_config(yaml.safe_load(file))
+        assert is_valid == True
+        assert errors == {}
 
 
 def test_validate_config_without_data_ingestion():
-    is_valid, errors = validate_config(
-        yaml.safe_load(open("./config_test/config_3_P.yaml", "r"))
-    )
-    assert is_valid == True
-    assert errors == {}
+    with open(os.path.join(test_path, "config_3_P.yaml"), "r") as file:
+        is_valid, errors = validate_config(yaml.safe_load(file))
+        assert is_valid == True
+        assert errors == {}
 
 
 def test_validate_config_missing_parameter():
-    is_valid, errors = validate_config(
-        yaml.safe_load(open("./config_test/config_4_F.yaml", "r"))
-    )
-    assert is_valid == False
-    assert errors == {
-        "index_clean_up_age_days": ["required field"],
-        "index_roll_over_size": ["required field"],
-    }
+    with open(os.path.join(test_path, "config_4_F.yaml"), "r") as file:
+        is_valid, errors = validate_config(yaml.safe_load(file))
+        assert is_valid == False
+        assert errors == {
+            "index_clean_up_age_days": ["required field"],
+            "index_roll_over_size": ["required field"],
+        }
 
 
 def test_validate_config_invalid_data_type():
-    is_valid, errors = validate_config(
-        yaml.safe_load(open("./config_test/config_5_F.yaml", "r"))
-    )
-    assert is_valid == False
-    assert errors == {
-        "data_ingestion": [
-            {
-                "states": [
-                    {3: [{"ingestion_rate_gb_per_hr": ["must be of number type"]}]}
-                ]
-            }
-        ]
-    }
+    with open(os.path.join(test_path, "config_5_F.yaml"), "r") as file:
+        is_valid, errors = validate_config(yaml.safe_load(file))
+        assert is_valid == False
+        assert errors == {
+            "data_ingestion": [
+                {
+                    "states": [
+                        {3: [{"ingestion_rate_gb_per_hr": ["must be of number type"]}]}
+                    ]
+                }
+            ]
+        }
 
 
 def test_validate_config_missing_nested_key():
-    is_valid, errors = validate_config(
-        yaml.safe_load(open("./config_test/config_6_F.yaml", "r"))
+    with open(os.path.join(test_path, "config_6_F.yaml"), "r") as file:
+        is_valid, errors = validate_config(yaml.safe_load(file))
+        assert is_valid == False
+        assert errors == {"searches": [{0: [{"probability": ["required field"]}]}]}
+
+
+def test_parse_config():
+    fp = open(os.path.join(test_path, "config_1_P.yaml"), "r")
+    all_configs = yaml.safe_load(fp.read())
+
+    data_generation_interval_minutes = all_configs.pop(
+        const.DATA_GENERATION_INTERVAL_MINUTES
     )
-    assert is_valid == False
-    assert errors == {"searches": [{0: [{"probability": ["required field"]}]}]}
+    data_ingestion = all_configs.pop(const.DATA_INGESTION)
+    searches = all_configs.pop(const.SEARCHES)
+    stats = all_configs
+
+    expected_config = Config(
+        stats, data_ingestion, searches, data_generation_interval_minutes
+    )
+    config = parse_config(os.path.join(test_path, "config_1_P.yaml"))
+    assert (
+        expected_config.data_generation_interval_minutes
+        == config.data_generation_interval_minutes
+    )
+    assert expected_config.stats == config.stats
+    assert expected_config.data_ingestion == config.data_ingestion
+    assert expected_config.searches == config.searches
+
+
+def test_parse_config_error_reading_config():
+    with pytest.raises(ValidationError) as e:
+        parse_config(os.path.join(test_path, "config_5_F.yaml"))
+        assert "error reading config file - " == e
+
+
+def test_parse_config_validate_error():
+    with pytest.raises(ValidationError) as e:
+        parse_config(os.path.join(test_path, "config_4_F.yaml"))
+        assert "Error validating config file - " == e
